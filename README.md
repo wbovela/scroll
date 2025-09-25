60249d7233a712b312e1d280ce35dbef20823678
Wouter Bovelander <wbovela@gmail.com>
2025-09-25 23:10:25 +0200

    Changed README.md to reflect current changes
    Completely rewrote scroll.asm to actually include colour.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 # scroll
-full screen scrolling commodore64
+full screen scrolling commodore64 (wip)
 
-This code demonstrates how to do a smooth scroll of an entire screen in character mode, including colour. This means moving an entire screen AND its colour ram after the screen has scrolled one whole character. To avoid flickering the routine applies double buffering meaning keeping a working on one half of the screen while the other is being drawn. 
+Code written in ACME style assembly, specific macro style for C64Studio IDE.
+This is work in progress.
+
+This code demonstrates how to do a smooth scroll of an entire screen in character mode, including colour to the left and right.
+
+Currently the code just repeats what falls off one side of the screen and copies it to the other side. 
+
+This code uses macros to use unrolled code for copying data. Unrolled code means that what was previously a classic loop has been written out completely without the use of a loop. This results in faster execution, but more code. Using macros is a way of shortening the notation of these lengthy unrolled loops.
+
+The waitFrame route causes the CPU to be busy waiting until the current raster line is at the bottom of the visible screen. That way changes can be made to the displayed data (at $0400 and $d800 for instance) without the screen being drawn. If the work to be done is more than what the bottom and top borders would allow, the waitFrame routine can be called to divide the work over more than 1 frame. 
+
+
--- scroll.asm
+++ scroll.asm
@@ -1,6 +1,40 @@
 ;compile to this filename
 !to "jmain.prg",cbm
 
+;macros
+!macro first_to_backup_column .rows {
+    !for .i = 0 to .rows-1 
+        lda SCREEN_CHAR + (.i * 40)
+        sta BACKUP_COLUMN + .i
+    !end
+}
+
+
+!macro backup_to_last_column .rows {
+  !for .i = 0 to .rows-1 
+    lda BACKUP_COLUMN + .i
+    sta SCREEN_CHAR + (.i * 40) + 39
+  !end
+}
+
+!macro scroll_color_ram .startrow, .endrow {
+  !for .i = .startrow to .endrow-1 
+    !for .j = 0 to 39 
+      lda SCREEN_COLOR + (.i * 40) + (.j + 1)
+      sta SCREEN_COLOR + (.i * 40) + .j
+    !end
+  !end
+}
+
+!macro scroll_char_ram .startrow, .endrow {
+  !for .i = .startrow to .endrow-1 
+    !for .j = 0 to 39 
+      lda SCREEN_CHAR + (.i * 40) + (.j + 1)
+      sta SCREEN_CHAR + (.i * 40) + .j
+    !end
+  !end
+}
+
 ;define constants here
 
 ;placeholder for various temp parameters
@@ -20,6 +54,14 @@ VIC_SCREENCTRL2         = $d016
 VIC_BORDER_COLOR        = $d020
 VIC_BACKGROUND_COLOR    = $d021
 
+JOYSTICK_PORT_II        = $dc00
+; all active low
+; 0001 (1)= up
+; 0010 (2)= down
+; 0100 (4)= left
+; 1000 (8)= right
+; 0001 0000 (16) = fire
+
 CIA_PRA                 = $dd00
 
 ;address of the screen buffer
@@ -28,86 +70,41 @@ SCREEN_CHAR             = $0400
 ;address of color ram
 SCREEN_COLOR            = $D800
 
-;address of the screen backup memory
-SCREEN_BACK_CHAR        = $2000
-
-;address of the screen backbuffer
-SCREEN_BACK_COLOR       = $C800
-
-;values for base and backup screen
-BASE_SCREEN             = $00
-BACKUP_SCREEN           = $FF
-
 ;set number of loops to delay scrolling
-SCROLL_DELAY_COUNT		= $01
+SCROLL_DELAY_COUNT    = $02
 
 ;this creates a basic start
 *=$0801
 
-		;SYS 2064
-		!byte $0C,$08,$0A,$00,$9E,$20,$32,$30,$36,$34,$00,$00,$00,$00,$00
+    ;SYS 2064
+    !byte $0C,$08,$0A,$00,$9E,$20,$32,$30,$36,$34,$00,$00,$00,$00,$00
 
-		; make the character colour white
-		ldy  #$00
-		lda  #$01
-.loopWhite
-		sta  SCREEN_COLOR,y
-		sta	SCREEN_CHAR,y
-		sta  SCREEN_COLOR+250,y
-		sta	SCREEN_CHAR+250,y
-		sta  SCREEN_COLOR+500,y
-		sta	SCREEN_CHAR+500,y
-		sta  SCREEN_COLOR+750,y
-		sta	SCREEN_CHAR+750,y
-		iny
-		cpy	#250
-		bne  .loopWhite
+    jsr initDisplay
 
-		;background black
-		lda #0
-		sta VIC_BACKGROUND_COLOR
-		sta VIC_BORDER_COLOR
+    ;background black
+    lda #0
+    sta VIC_BACKGROUND_COLOR
+    sta VIC_BORDER_COLOR
 
-		; set 38 column mode
-		lda VIC_SCREENCTRL2
-		and #%11110111
-		sta VIC_SCREENCTRL2
+    ; set 38 column mode
+    lda VIC_SCREENCTRL2
+    and #%11110111  ; 0-2 = fine scroll, 3 = 38/40 columns mode, 4 = multicolor mode, 5-7 unused
+    sta VIC_SCREENCTRL2
 
-		; set scroll delay to 0
-		ldx  #$00
-		stx  SCROLL_DELAY
+    ; set scroll delay to 0
+    ldx  #$00
+    stx  SCROLL_DELAY
 
-		; start at 7 for left scrolling
-		lda  #$07
-		sta  SCROLL_POS
+    ; start at 7 for left scrolling
+    lda  #$07
+    sta  SCROLL_POS
 
-		; set scroll position to 7
-		lda VIC_SCREENCTRL2
-		and #$F8
-		clc 
-		adc SCROLL_POS
-		sta VIC_SCREENCTRL2   
-
-		; set base screen as active screen
-		lda  #BASE_SCREEN
-		sta  ACTIVE_SCREEN 
-
-		; start off by making a copy of the current screen to the backup
-		jsr  copyBaseToBackup
-
-		; set up the Zeropage pointers for the initial situation
-		; ZP3 and ZP4 are used for hard scrolling the screen.
-		; set zeropage pointer 3 to backup screen line offset low table
-		lda  #<SCREEN_BACK_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3
-		lda  #>SCREEN_BACK_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3+1
-
-		; set zeropage pointer 4 to backup screen line offset high table
-		lda  #<SCREEN_BACK_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4
-		lda  #>SCREEN_BACK_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4+1          
+    ; set scroll position to SCROLL_POS
+    lda VIC_SCREENCTRL2
+    and #$F8
+    clc 
+    adc SCROLL_POS
+    sta VIC_SCREENCTRL2   
 
 ;------------------------------------------------------------
 ;
@@ -116,18 +113,61 @@ SCROLL_DELAY_COUNT		= $01
 ;------------------------------------------------------------
 !zone GameLoop
 GameLoop  
-		jsr waitFrame
+    jsr waitFrame
+    
+    lda #$04
+    bit JOYSTICK_PORT_II
+    bne .noLeft
 
-		;lda #1
-		;sta VIC_BORDER_COLOR
+    jsr  softScrollLeft
 
-		jsr  softScrollLeft
+.noLeft
 
-		;lda #0
-		;sta VIC_BORDER_COLOR
+    lda #$08
+    bit JOYSTICK_PORT_II
+    bne .noRight
 
-		jmp  GameLoop         
-	
+    ;jsr softScrollRight
+
+.noRight
+
+    jmp  GameLoop         
+  
+;------------------------------------------------------------ 
+;
+; initDisplay
+;
+;------------------------------------------------------------
+
+!zone initDisplay
+initDisplay
+    ; set character colour 
+    ldy  #$00
+    lda  #$01
+.loopCharColour
+    sta  SCREEN_COLOR,y
+    sta  SCREEN_COLOR+250,y
+    sta  SCREEN_COLOR+500,y
+    sta  SCREEN_COLOR+750,y
+    iny
+    tya       ; increase colour
+    cpy #250
+    bne  .loopCharColour
+
+    ; set characters to A
+    ldy  #$00
+    lda  #$01
+.loopChar
+    sta SCREEN_CHAR,y
+    sta SCREEN_CHAR+250,y
+    sta SCREEN_CHAR+500,y
+    sta SCREEN_CHAR+750,y
+    iny
+    tya       ; increase character
+    cpy #250
+    bne  .loopChar
+    rts
+
 ;------------------------------------------------------------
 ;
 ;    softScrollLeft
@@ -135,249 +175,64 @@ GameLoop
 ;------------------------------------------------------------          
 !zone softScrollLeft
 softScrollLeft
-		;check whether to execute the scroll
-		ldx  SCROLL_DELAY
-		cpx  #SCROLL_DELAY_COUNT
-		beq  .doScroll
-		  
-		inc  SCROLL_DELAY
-		rts
+    ;check whether to execute the scroll
+    ldx  SCROLL_DELAY
+    cpx  #SCROLL_DELAY_COUNT
+    beq  .doScroll
+      
+    inc  SCROLL_DELAY
+    rts
 
 .doScroll
-		; let's scroll a pixel
-		; reset the scroll delay to zero
-		ldx	#$00
-		stx	SCROLL_DELAY
+    
+    ; reset the scroll delay to zero
+    ldx #$00
+    stx SCROLL_DELAY
 
-		; are we at pixel 2 yet?
-		ldx  SCROLL_POS
-		cpx  #$02
-		bne  .notattwo
+    lda SCROLL_POS
+    bne .notatzero
 
-		; set start row
-		lda  #13
-		sta  PARAM2
-		; set end row
-		lda  #24
-		sta  PARAM3
+    jsr hardScrollScreen
 
-		; now call the hard scroll routine 
-		jsr  hardScrollScreen          
-
-		jmp  .notatfour          
-
-.notattwo
-		; check if we're at pixel 4
-		cpx  #$04
-		bne  .notatfour
-
-		; set start row
-		lda  #0
-		sta  PARAM2
-		; set end row
-		lda  #12
-		sta  PARAM3       
-
-		; now call the hard scroll routine 
-		jsr  hardScrollScreen
-
-.notatfour
-		; scroll left one pixel until SCROLL_POS = $FF
-		dec  SCROLL_POS
-		ldx  SCROLL_POS
-		bpl  .setScrollRegister
+.notatzero
+    ; scroll left one pixel until SCROLL_POS = $FF
+    dec  SCROLL_POS
+    ldx  SCROLL_POS
+    bpl  .setScrollRegister
 
 .resetPosition
-		; Switch to scroll pos 7
-		; Switch screen between base<->backup
-		; Copy the contents between base<->backup
-		lda  #$07
-		sta  SCROLL_POS
-
-		; test the current active screen 0=base, $FF=backup
-		lda  ACTIVE_SCREEN
-		beq  .switch_to_backup
-
-.switch_to_base
-		; load the current value, clear bits #0-#2, add scroll position and write back
-		lda VIC_SCREENCTRL2
-		and #$F8
-		clc 
-		adc SCROLL_POS
-		sta VIC_SCREENCTRL2
-
-		; the new visible area will be the base area
-		jsr  SetVideoRamToBase
-
-		; set base screen as active
-		lda  #BASE_SCREEN
-		sta  ACTIVE_SCREEN
-
-		; copy the hard scrolled information to backup
-		jsr  copyBaseToBackup
-
-		; set zeropage pointer 3 to backup screen line offset low table
-		lda  #<SCREEN_BACK_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3
-		lda  #>SCREEN_BACK_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3+1
-
-		; set zeropage pointer 4 to backup screen line offset high table
-		lda  #<SCREEN_BACK_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4
-		lda  #>SCREEN_BACK_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4+1          
-
-		jmp  .exit
-
-.switch_to_backup
-		; load the current value, clear bits #0-#2, add scroll position and write back
-		lda VIC_SCREENCTRL2
-		and #$F8
-		clc 
-		adc SCROLL_POS
-		sta VIC_SCREENCTRL2
-
-		; the new visible area will be the backup area
-		jsr  SetVideoRamToBackup
-
-		; set backup screen as active
-		lda  #BACKUP_SCREEN
-		sta  ACTIVE_SCREEN
-
-		; copy the hard scrolled data to the base area
-		jsr  copyBackupToBase
-
-		; set zeropage pointer 3 to base screen line offset low table
-		lda  #<SCREEN_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3
-		lda  #>SCREEN_LINE_OFFSET_TABLE_LO
-		sta  ZEROPAGE_POINTER_3+1
-
-		; set zeropage pointer 4 to base screen line offset high table
-		lda  #<SCREEN_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4
-		lda  #>SCREEN_LINE_OFFSET_TABLE_HI
-		sta  ZEROPAGE_POINTER_4+1          
+    ; Switch to scroll pos 7
+    lda  #$07
+    sta  SCROLL_POS
 
 .setScrollRegister
-		; load the current value, clear bits #0-#2, add scroll position and write back
-		lda VIC_SCREENCTRL2
-		and #$F8
-		clc 
-		adc SCROLL_POS
-		sta VIC_SCREENCTRL2
-
-.exit
-		rts
+    ; load the current value, clear bits #0-#2, add scroll position and write back
+    lda VIC_SCREENCTRL2
+    and #$F8
+    clc 
+    adc SCROLL_POS
+    sta VIC_SCREENCTRL2
+    rts
 
 ;------------------------------------------------------------
 ;
 ;    hardScrollScreen
-;    ZEROPAGE_POINTER_3 = address of line offset table low 
-;    ZEROPAGE_POINTER_4 = address of line offset table high
 ;    PARAM2 = start row
 ;    PARAM3 = end row
 ;------------------------------------------------------------          
 !zone hardScrollScreen
 hardScrollScreen
-		
-		; copy the first column to backup table
-		ldy  #$00
-		
-		; take address of backup column
-		lda  #<BACKUP_COLUMN
-		sta  ZEROPAGE_POINTER_2
-		lda  #>BACKUP_COLUMN
-		sta  ZEROPAGE_POINTER_2+1		
-.loop
-		; take address of first character on line y
-		lda  (ZEROPAGE_POINTER_3),y
-		sta  ZEROPAGE_POINTER_1
-		lda  (ZEROPAGE_POINTER_4),y
-		sta  ZEROPAGE_POINTER_1+1
+    +first_to_backup_column 10   ; generates the 25 lda/sta pairs
 
-		; take 1st character on line y and store in table at position y
-		sty  PARAM1
-		  
-		ldy  PARAM2
-		lda  (ZEROPAGE_POINTER_1),y
-		ldy  PARAM1
-		sta  (ZEROPAGE_POINTER_2),y
+    ; now we shift all columns on each row left by one character
+    ;+scroll_char_ram 10, 20
+    +scroll_char_ram 0, 10
 
-		; do this for lines PARAM2 to PARAM3
-		iny
-		cpy  PARAM3
-		bne  .loop
+    +backup_to_last_column 10 
 
-		; now we shift all columns on each row left by one character
-		ldy  PARAM2
+    +scroll_color_ram 0,10
 
-.nextRow
-		; take address of first character on line y
-		lda  (ZEROPAGE_POINTER_3),y
-		sta  ZEROPAGE_POINTER_1
-		lda  (ZEROPAGE_POINTER_4),y
-		sta  ZEROPAGE_POINTER_1+1          
-
-		sty  PARAM1
-
-		; y=column 0
-		ldy  #$00
-
-.nextColumn
-		; take the character from column y+1 and store in column y
-		iny
-		lda  (ZEROPAGE_POINTER_1),y
-		dey
-		sta  (ZEROPAGE_POINTER_1),y
-
-		; next column
-		iny
-
-		; stop at column 40
-		cpy  #39
-		bne  .nextColumn
-
-		; now do the next row, so get the row number from PARAM1 and increase it
-		ldy  PARAM1
-		iny
-
-		; stop after PARAM3 rows
-		cpy  PARAM3
-		bne  .nextRow
-
-.rowsDone
-
-		; copy the backup column to the last column
-		ldy  PARAM2
-.loopLastColumn
-		; take address of first character on line y
-		lda  (ZEROPAGE_POINTER_3),y
-		sta  ZEROPAGE_POINTER_1
-		lda  (ZEROPAGE_POINTER_4),y
-		sta  ZEROPAGE_POINTER_1+1
-
-		; take address of backup column
-		lda  #<BACKUP_COLUMN
-		sta  ZEROPAGE_POINTER_2
-		lda  #>BACKUP_COLUMN
-		sta  ZEROPAGE_POINTER_2+1
-
-		; take 1st character on line y and store in table at position 39
-		lda  (ZEROPAGE_POINTER_2),y
-
-		sty  PARAM1
-		ldy  #39
-		sta  (ZEROPAGE_POINTER_1),y
-		ldy  PARAM1
-
-		; do this for lines PARAM2 to PARAM3
-		iny
-		cpy  PARAM3
-		bne  .loopLastColumn
-
-		rts
+    rts
 
 ;---------------------------------------
 ;
@@ -385,97 +240,28 @@ hardScrollScreen
 ;
 ;---------------------------------------
 !zone waitFrame
-		;wait for the raster to reach line $f8
-		;this is keeping our timing stable
-		  
-		;are we on line $F8 already? if so, wait for the next full screen
-		;prevents mistimings if called too fast
+    ;wait for the raster to reach line $f8
+    ;this is keeping our timing stable
+      
+    ;are we on line $F8 already? if so, wait for the next full screen
+    ;prevents mistimings if called too fast
 waitFrame 
-		lda $d012
-		cmp #$F8
-		beq waitFrame
+    lda $d012
+    cmp #$F8
+    beq waitFrame
 
-		;wait for the raster to reach line $f8 (should be closer to the start of this line this way)
+    ;wait for the raster to reach line $f8 (should be closer to the start of this line this way)
 .WaitStep2
-		lda $d012
-		cmp #$F8
-		bne .WaitStep2
-		  
-		rts
+    lda $d012
+    cmp #$F8
+    bne .WaitStep2
+      
+    rts
+
 
 ;---------------------------------------
 ;
-;    copyBaseToBackup
-;
-;---------------------------------------
-!zone copyBaseToBackup
-copyBaseToBackup
-		ldy  #$00
-.loop
-		lda  SCREEN_CHAR,y
-		sta  SCREEN_BACK_CHAR,y
-		lda  SCREEN_CHAR+250,y
-		sta  SCREEN_BACK_CHAR+250,y
-		lda  SCREEN_CHAR+500,y
-		sta  SCREEN_BACK_CHAR+500,y
-		lda  SCREEN_CHAR+750,y
-		sta  SCREEN_BACK_CHAR+750,y
-		iny
-		cpy  #250
-		bne  .loop
-
-		rts
-
-;---------------------------------------
-;    copyBackupToBase
-;---------------------------------------
-!zone copyBackupToBase
-copyBackupToBase
-		ldy  #$00
-.loop
-		lda  SCREEN_BACK_CHAR,y
-		sta  SCREEN_CHAR,y
-		lda  SCREEN_BACK_CHAR+250,y
-		sta  SCREEN_CHAR+250,y
-		lda  SCREEN_BACK_CHAR+500,y
-		sta  SCREEN_CHAR+500,y
-		lda  SCREEN_BACK_CHAR+750,y
-		sta  SCREEN_CHAR+750,y
-		iny
-		cpy  #250
-		bne  .loop
-
-		rts
-
-;---------------------------------------
-;
-;    SetVideoRamToBase
-;    $0400
-;---------------------------------------
-!zone SetVideoRamToBase
-SetVideoRamToBase
-		lda $d018
-		and #$0f
-		ora #$10
-		sta $d018
-		rts
-
-;---------------------------------------
-;
-;    SetVideoRamToBackup
-;    $2000
-;---------------------------------------
-!zone SetVideoRamToBackup
-SetVideoRamToBackup
-		lda $d018
-		and #$0f
-		ora #$80
-		sta $d018
-		rts
-
-;---------------------------------------
-;
-;	Game data goes here
+; Game data goes here
 ;
 ;---------------------------------------
 
@@ -483,119 +269,9 @@ SetVideoRamToBackup
 BACKUP_COLUMN  !fill     25     
 
 ; the delay counter for scrolling
-SCROLL_DELAY	!byte	0
+SCROLL_DELAY  !byte 0
 
 ; the current horizontal sroll position
 SCROLL_POS     !byte     0
 
-; indicated active screen $0=base, $ff=backup
-ACTIVE_SCREEN  !byte     0
-		 
-; tables of address of first character on each line of base and backup screens (low and high parts)
-SCREEN_LINE_OFFSET_TABLE_LO
-		!byte ( SCREEN_CHAR +   0 ) & 0x00ff
-		!byte ( SCREEN_CHAR +  40 ) & 0x00ff
-		!byte ( SCREEN_CHAR +  80 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 120 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 160 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 200 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 240 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 280 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 320 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 360 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 400 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 440 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 480 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 520 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 560 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 600 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 640 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 680 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 720 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 760 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 800 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 840 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 880 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 920 ) & 0x00ff
-		!byte ( SCREEN_CHAR + 960 ) & 0x00ff
-		  
-SCREEN_LINE_OFFSET_TABLE_HI
-		!byte ( ( SCREEN_CHAR +   0 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR +  40 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR +  80 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 120 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 160 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 200 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 240 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 280 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 320 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 360 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 400 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 440 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 480 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 520 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 560 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 600 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 640 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 680 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 720 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 760 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 800 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 840 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 880 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 920 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_CHAR + 960 ) & 0xff00 ) >> 8
- 
-SCREEN_BACK_LINE_OFFSET_TABLE_LO
-		!byte ( SCREEN_BACK_CHAR +   0 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR +  40 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR +  80 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 120 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 160 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 200 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 240 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 280 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 320 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 360 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 400 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 440 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 480 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 520 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 560 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 600 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 640 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 680 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 720 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 760 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 800 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 840 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 880 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 920 ) & 0x00ff
-		!byte ( SCREEN_BACK_CHAR + 960 ) & 0x00ff
-
-SCREEN_BACK_LINE_OFFSET_TABLE_HI
-		!byte ( ( SCREEN_BACK_CHAR +   0 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR +  40 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR +  80 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 120 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 160 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 200 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 240 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 280 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 320 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 360 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 400 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 440 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 480 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 520 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 560 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 600 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 640 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 680 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 720 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 760 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 800 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 840 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 880 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 920 ) & 0xff00 ) >> 8
-		!byte ( ( SCREEN_BACK_CHAR + 960 ) & 0xff00 ) >> 8
+     
